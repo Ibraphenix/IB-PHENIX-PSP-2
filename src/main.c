c4dd0543a07f1d1a1d2dcaa0a28eb7a7800128c7
Ibraphenix <ibrahimdenkey77@gmail.com>
2026-08-20 18:17:35 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -104,22 +104,55 @@ void drawPlayer(int x, int y)
     pspDebugScreenPrintf("/___\\");
 }
 
-void drawGame(int playerX, int playerY)
+void drawEnemy(int x, int y)
+{
+    pspDebugScreenSetXY(x, y);
+    pspDebugScreenPrintf(" /\\ ");
+
+    pspDebugScreenSetXY(x, y + 1);
+    pspDebugScreenPrintf("<XX>");
+
+    pspDebugScreenSetXY(x, y + 2);
+    pspDebugScreenPrintf(" \\/ ");
+
+    pspDebugScreenSetXY(x, y + 3);
+    pspDebugScreenPrintf("/  \\");
+}
+
+void drawHealth(int health)
+{
+    pspDebugScreenSetXY(2, 2);
+    pspDebugScreenPrintf("HP: ");
+
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (i < health)
+            pspDebugScreenPrintf("#");
+        else
+            pspDebugScreenPrintf("-");
+    }
+
+    pspDebugScreenPrintf(" %d/10", health);
+}
+
+void drawGame(
+    int playerX,
+    int playerY,
+    int enemyX,
+    int enemyY,
+    int health
+)
 {
     pspDebugScreenClear();
 
     pspDebugScreenSetXY(2, 1);
     pspDebugScreenPrintf("IB PHENIX - ZONE 01");
 
-    pspDebugScreenSetXY(2, 3);
-    pspDebugScreenPrintf("THE PHOENIX AWAKENS");
+    drawHealth(health);
 
     pspDebugScreenSetXY(2, 5);
-    pspDebugScreenPrintf(
-        "=========================================================="
-    );
-
-    pspDebugScreenSetXY(2, 20);
     pspDebugScreenPrintf(
         "=========================================================="
     );
@@ -129,10 +162,10 @@ void drawGame(int playerX, int playerY)
 
     pspDebugScreenSetXY(5, 11);
     pspDebugScreenPrintf("      ^       ^       ^");
-    
+
     pspDebugScreenSetXY(5, 12);
     pspDebugScreenPrintf("     /|\\     /|\\     /|\\");
-    
+
     pspDebugScreenSetXY(5, 13);
     pspDebugScreenPrintf("    /###\\   /###\\   /###\\");
 
@@ -140,12 +173,16 @@ void drawGame(int playerX, int playerY)
     pspDebugScreenPrintf("              PATH");
 
     drawPlayer(playerX, playerY);
+    drawEnemy(enemyX, enemyY);
 
     pspDebugScreenSetXY(2, 22);
     pspDebugScreenPrintf("ANALOG / D-PAD : MOVE");
 
     pspDebugScreenSetXY(2, 24);
-    pspDebugScreenPrintf("O : MENU       START : EXIT");
+    pspDebugScreenPrintf("X : ATTACK     O : MENU");
+
+    pspDebugScreenSetXY(2, 25);
+    pspDebugScreenPrintf("START : EXIT");
 }
 
 void playGame(void)
@@ -154,8 +191,16 @@ void playGame(void)
 
     unsigned int oldButtons = 0;
 
-    int playerX = 25;
+    int playerX = 10;
     int playerY = 12;
+
+    int enemyX = 42;
+    int enemyY = 12;
+
+    int health = 10;
+
+    int attackTimer = 0;
+    int damageTimer = 0;
 
     int running = 1;
 
@@ -202,13 +247,73 @@ void playGame(void)
         if (playerY > 15)
             playerY = 15;
 
+        if (enemyX < playerX)
+            enemyX++;
+
+        if (enemyX > playerX)
+            enemyX--;
+
+        if (enemyY < playerY)
+            enemyY++;
+
+        if (enemyY > playerY)
+            enemyY--;
+
+        if (pressed & PSP_CTRL_CROSS)
+        {
+            attackTimer = 10;
+
+            if (enemyX >= playerX - 3 &&
+                enemyX <= playerX + 7 &&
+                enemyY >= playerY - 3 &&
+                enemyY <= playerY + 5)
+            {
+                enemyX = 42;
+                enemyY = 12;
+            }
+        }
+
+        if (attackTimer > 0)
+            attackTimer--;
+
+        if (damageTimer > 0)
+            damageTimer--;
+
+        if (enemyX >= playerX - 2 &&
+            enemyX <= playerX + 5 &&
+            enemyY >= playerY - 2 &&
+            enemyY <= playerY + 4)
+        {
+            if (damageTimer == 0)
+            {
+                health--;
+
+                damageTimer = 30;
+
+                if (health <= 0)
+                {
+                    health = 10;
+                    playerX = 10;
+                    playerY = 12;
+                    enemyX = 42;
+                    enemyY = 12;
+                }
+            }
+        }
+
         if (pressed & PSP_CTRL_CIRCLE)
             running = 0;
 
         if (pressed & PSP_CTRL_START)
             sceKernelExitGame();
 
-        drawGame(playerX, playerY);
+        drawGame(
+            playerX,
+            playerY,
+            enemyX,
+            enemyY,
+            health
+        );
 
         oldButtons = pad.Buttons;
 
