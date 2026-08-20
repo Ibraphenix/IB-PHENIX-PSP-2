f84de8a1d52ee966d55c83e53d823e50e87b4582
Ibraphenix <ibrahimdenkey77@gmail.com>
2026-08-20 14:06:04 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -45,6 +45,50 @@ int SetupCallbacks(void)
     return thid;
 }
 
+void draw_menu(int selected)
+{
+    pspDebugScreenClear();
+
+    pspDebugScreenSetXY(8, 3);
+    pspDebugScreenPrintf("================================");
+
+    pspDebugScreenSetXY(10, 5);
+    pspDebugScreenPrintf("IB PHENIX");
+
+    pspDebugScreenSetXY(9, 6);
+    pspDebugScreenPrintf("PSP PROTOTYPE");
+
+    pspDebugScreenSetXY(7, 9);
+
+    if (selected == 0)
+        pspDebugScreenPrintf("> COMMENCER L'AVENTURE");
+    else
+        pspDebugScreenPrintf("  COMMENCER L'AVENTURE");
+
+    pspDebugScreenSetXY(7, 11);
+
+    if (selected == 1)
+        pspDebugScreenPrintf("> OPTIONS");
+    else
+        pspDebugScreenPrintf("  OPTIONS");
+
+    pspDebugScreenSetXY(7, 13);
+
+    if (selected == 2)
+        pspDebugScreenPrintf("> QUITTER");
+    else
+        pspDebugScreenPrintf("  QUITTER");
+
+    pspDebugScreenSetXY(6, 18);
+    pspDebugScreenPrintf("UP/DOWN : NAVIGATE");
+
+    pspDebugScreenSetXY(6, 19);
+    pspDebugScreenPrintf("X : SELECT     O : BACK");
+
+    pspDebugScreenSetXY(6, 20);
+    pspDebugScreenPrintf("START : EXIT");
+}
+
 int main(void)
 {
     SetupCallbacks();
@@ -55,23 +99,92 @@ int main(void)
     sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
 
     SceCtrlData pad;
+    unsigned int oldButtons = 0;
 
-    while (1)
+    int selected = 0;
+    int running = 1;
+
+    draw_menu(selected);
+
+    while (running)
     {
         sceCtrlPeekBufferPositive(&pad, 1);
 
-        pspDebugScreenSetXY(0, 5);
+        unsigned int pressed =
+            pad.Buttons & ~oldButtons;
 
-        pspDebugScreenPrintf("IB PHENIX CONTROLLER TEST\n\n");
-        pspDebugScreenPrintf("BUTTONS : %08X\n", pad.Buttons);
-        pspDebugScreenPrintf("ANALOG X: %3d\n", pad.Lx);
-        pspDebugScreenPrintf("ANALOG Y: %3d\n", pad.Ly);
+        if (pressed & PSP_CTRL_UP)
+        {
+            selected--;
 
-        if (pad.Buttons & PSP_CTRL_START)
-            sceKernelExitGame();
+            if (selected < 0)
+                selected = 2;
+
+            draw_menu(selected);
+        }
+
+        if (pressed & PSP_CTRL_DOWN)
+        {
+            selected++;
+
+            if (selected > 2)
+                selected = 0;
+
+            draw_menu(selected);
+        }
+
+        if (pressed & PSP_CTRL_CROSS)
+        {
+            if (selected == 0)
+            {
+                pspDebugScreenClear();
+
+                pspDebugScreenSetXY(8, 7);
+                pspDebugScreenPrintf("IB PHENIX");
+
+                pspDebugScreenSetXY(6, 9);
+                pspDebugScreenPrintf("AVENTURE EN PREPARATION...");
+
+                pspDebugScreenSetXY(6, 12);
+                pspDebugScreenPrintf("Appuie sur O pour revenir.");
+            }
+
+            if (selected == 1)
+            {
+                pspDebugScreenClear();
+
+                pspDebugScreenSetXY(8, 7);
+                pspDebugScreenPrintf("OPTIONS");
+
+                pspDebugScreenSetXY(5, 10);
+                pspDebugScreenPrintf("Prototype IB PHENIX");
+
+                pspDebugScreenSetXY(6, 14);
+                pspDebugScreenPrintf("Appuie sur O pour revenir.");
+            }
+
+            if (selected == 2)
+            {
+                running = 0;
+            }
+        }
+
+        if (pressed & PSP_CTRL_CIRCLE)
+        {
+            draw_menu(selected);
+        }
+
+        if (pressed & PSP_CTRL_START)
+        {
+            running = 0;
+        }
+
+        oldButtons = pad.Buttons;
 
         sceDisplayWaitVblankStart();
     }
+
+    sceKernelExitGame();
 
     return 0;
 }
